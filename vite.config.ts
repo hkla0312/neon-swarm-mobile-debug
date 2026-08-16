de59ba3817831c5a118c369127eaaf1caab7026e
hkla0312 <h.k0312.dr@gmail.com>
2026-08-17 07:27:14 +0900

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  server: { host: '0.0.0.0', port: 53173, strictPort: true },
+  preview: { host: '0.0.0.0', port: 54173, strictPort: true },
+  optimizeDeps: { noDiscovery: true, include: [] }
+});
+
